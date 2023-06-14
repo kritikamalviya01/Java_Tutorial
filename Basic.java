eeaf61f76adcffe368fc2d5933d1fba137854d07
Kritika Malviya <66256137+kritikamalviya01@users.noreply.github.com>
2023-06-14 15:38:35 +0530

    Java Tutorial
    First Commit

---
+++ Basic.java
@@ -0,0 +1,16 @@
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        //Variables
+        int a = 10;
+        double b = -23456789.;
+        float c = 2.344f;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+    }
+}
